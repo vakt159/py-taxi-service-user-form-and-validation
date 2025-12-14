c2c50833fe7a1708686562bfbd6ef763b92a7888
Andrii Yermolenko <vakt159@gmail.com>
2025-12-14 19:49:37 +0200

    Fixed flake8 problem

--- taxi/views.py
+++ taxi/views.py
@@ -88,7 +88,8 @@ class DriverListView(LoginRequiredMixin, generic.ListView):
 
 class DriverDetailView(LoginRequiredMixin, generic.DetailView):
     model = get_user_model()
-    queryset = get_user_model().objects.all().prefetch_related("cars__manufacturer")
+    queryset = get_user_model().objects.all().prefetch_related(
+        "cars__manufacturer")
 
 
 class DriverCreateView(LoginRequiredMixin, generic.CreateView):
